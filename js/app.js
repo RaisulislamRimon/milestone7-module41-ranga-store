ef4f5dc2fe26946b4f612d0325b6731f4e9ff1d8
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-11 19:08:58 +0600

    update product name in modal

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ const showProductDetails = (product_id) => {
 };
 
 const showProductDetailsInModal = (product_details) => {
-  //   console.log(product_details);
+  console.log(product_details);
   setInnerText("exampleModalLabel", product_details.title);
   setInnerText("product_id", product_details.id);
   setInnerText("modal_body", product_details.description);
@@ -78,15 +78,18 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  //   document.getElementById(id).innerText = Math.round(total);
-  //   document.getElementById(id).innerText = parseFloat(total);
   document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
   //   document.getElementById(id).innerText = Math.round(value);
-  document.getElementById(id).innerText = parseFloat(value);
+  if (typeof value === "number") {
+    value = Math.round(value);
+  } else {
+    value = value;
+  }
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
